da86e5ec6ebf8b6a2feead2a17350a9781a546c9
Caleb Jacob <thecalebjacob@gmail.com>
2022-04-21 13:35:30 -0600

    Getting started

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# pf-parent
-Package Framework: A framework experiment using packages.
+# Package Framework (PF)
+
+A Next JS framework experiment using packages to orchestrate multiple modules over multiple teams with the best possible DX.
+
+## Getting Started
+
+Make sure you have NVM installed to manage your current version of Node and NPM:
+
+```
+brew install nvm
+```
+
+Once NVM has been installed, navigate to the project's root directory and run:
+
+```
+nvm install
+nvm use
+```
+
+Now install all of the project's dependencies via NPM:
+
+```
+npm install
+```
+
+Finally, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result. Hot reloading is set up to pull in changes whenever code is modified.
---
+++ pages/index.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+
+import { Button } from '@/components/button';
+import { Flex } from '@/components/flex';
+import { H1, H2, H3 } from '@/components/heading';
+import { P } from '@/components/paragraph';
+import { Section } from '@/components/section';
+import { TextLink } from '@/components/text-link';
+// import { CrazyThing } from '@/smf-child-1/components/crazy-thing';
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Section color="primary" noBorder>
+        <H1>Hello!</H1>
+
+        <P>
+          This is some paragraph text. Here is a{' '}
+          <Link href="/smf-child-1/crazy" passHref>
+            <TextLink>link</TextLink>
+          </Link>{' '}
+          to a submodule example.
+        </P>
+
+        <P>
+          THere is a different{' '}
+          <Link href="/smf-child-2/cool" passHref>
+            <TextLink>link</TextLink>
+          </Link>{' '}
+          to a submodule example.
+        </P>
+
+        <Flex stack="mobile">
+          <Button
+            size="large"
+            onClick={() => {
+              alert('Hello 1');
+            }}
+          >
+            Click Me
+          </Button>
+          <Button
+            color="secondary"
+            onClick={() => {
+              alert('Hello 2');
+            }}
+          >
+            Click Me
+          </Button>
+          <Button
+            color="secondary"
+            size="small"
+            onClick={() => {
+              alert('Hello 3');
+            }}
+          >
+            Click Me
+          </Button>
+        </Flex>
+      </Section>
+
+      <Section>
+        <H2>A Submodule Component</H2>
+        {/* <CrazyThing title="Hello There!"></CrazyThing> */}
+      </Section>
+
+      <Section>
+        <H3>Another Section</H3>
+        <P>This is some other paragraph text.</P>
+      </Section>
+    </>
+  );
+};
+
+export default HomePage;
